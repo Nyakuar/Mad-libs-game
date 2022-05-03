50080a2c002e8457cb0eab3ffffd9f405897de87
Nyakuar <nyakuaryy@gmail.com>
2022-05-03 21:40:55 +0300

    first commit

---
+++ quiz.py
@@ -0,0 +1,10 @@
+
+
+
+color = input("Enter a color: ")
+Plural_nuon = input("Enter a Pural noun: ")
+celebrity = input("Enter a celebrity ")
+
+print("Roses " + color )
+print(Plural_nuon  + " are Blue")
+print("I love you"  + celebrity)
